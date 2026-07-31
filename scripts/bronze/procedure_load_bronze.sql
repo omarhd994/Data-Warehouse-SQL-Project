2ae2d05ea8e13685c7b7138f10d6288f7e14e858
omarhd994 <wildsniper94@gmail.com>
2026-07-31 21:59:53 +0200

    Path added

--- scripts/bronze/procedure_load_bronze.sql
+++ scripts/bronze/procedure_load_bronze.sql
@@ -27,7 +27,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\---\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\Users\cwg_m\Desktop\sql-datawarehouse\Data-Warehouse-SQL-Project\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -43,7 +43,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\---\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\Users\cwg_m\Desktop\sql-datawarehouse\Data-Warehouse-SQL-Project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -58,7 +58,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\---\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Users\cwg_m\Desktop\sql-datawarehouse\Data-Warehouse-SQL-Project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -77,7 +77,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\---\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\Users\cwg_m\Desktop\sql-datawarehouse\Data-Warehouse-SQL-Project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -92,7 +92,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\---\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		FROM 'C:\Users\cwg_m\Desktop\sql-datawarehouse\Data-Warehouse-SQL-Project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -107,7 +107,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\---\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\Users\cwg_m\Desktop\sql-datawarehouse\Data-Warehouse-SQL-Project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
